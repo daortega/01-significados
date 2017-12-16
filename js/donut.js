6fd1aa99572f82e686095ee3a4ecae84bae2afc1
Denisse Ortega <daortega@uc.cl>
2017-12-15 21:43:17 -0300

    Update donut.js

--- js/donut.js
+++ js/donut.js
@@ -64,14 +64,14 @@ d3.csv("data/data.csv", function(error, data) {
             .data(function(d) { return pie(d.ages); })
             //Para añadir ToolTips o PopOver
           .enter().append("a")
-              .attr("class", "tooltip-test")
-              /*.attr("href", "#")
-              .attr("title", function(d) { return d.data.name; })
+              .attr("class", "tooltip-test") 
+                .attr("href", "#")
+             /* .attr("title", function(d) { return d.data.name; })
               .attr("data-toggle", "popover")
               .attr("data-trigger", "click")  //poner hover o click
               .attr("data-container", "section")
-              .attr("data-content", function(d) { return d.data.population; })
-          //POPOVER YEAAAHHH*/
+              .attr("data-content", function(d) { return d.data.population; })*/
+          //POPOVER YEAAAHHH
               .on('mouseover', function(d) {
                   $("#tooltip")
                     .html(d.data.name + ": " + d.data.population + " answers")
